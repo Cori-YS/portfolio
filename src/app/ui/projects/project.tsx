45b11d4a2c41060face055ef33a6f19a154192c9
Cori-YS <ismeliocori@hotmail.com>
2025-03-02 21:36:51 +0100

    fix: remove justification

--- src/app/ui/projects/project.tsx
+++ src/app/ui/projects/project.tsx
@@ -5,7 +5,7 @@ export function Project() {
     <div className='grid grid-cols-2 gap-0 w-72 h-44 bg-gray m-5 p-3 rounded-lg'>
       <h1 className='font-sans'>Project Name</h1>
       <FaNodeJs className='text-4xl justify-self-end' />
-      <p className='col-span-2 mt-0 pt-0 font-mono text-sm text-justify text-gray-text'>
+      <p className='col-span-2 mt-0 pt-0 font-mono text-sm text-gray-text'>
         Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
         tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim.
       </p>
