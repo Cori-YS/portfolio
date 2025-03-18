b222caf7ed2950e5f1f47ad92b2ccf1bedcc22ca
Cori-YS <ismeliocori@hotmail.com>
2025-03-18 21:29:13 +0100

    fix: progress bar

--- src/app/ui/skills/skill.tsx
+++ src/app/ui/skills/skill.tsx
@@ -5,7 +5,7 @@ export function Skill() {
     <div className='grid grid-cols-1 text-center bg-gray w-48 p-5 text-gray-text rounded-lg'>
       <FaNodeJs className='text-8xl mx-auto' />
       <span className='font-mono mb-1'>Node Js</span>
-      <progress value={70} max={100} className='h-1 mx-auto'></progress>
+      <progress value={70} max={100} className='h-1'></progress>
     </div>
   );
 }
