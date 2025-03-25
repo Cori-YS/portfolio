9779b90ffb750b7285ef2fb20bbeb8a896331bf4
Cori-YS <ismeliocori@hotmail.com>
2025-03-25 20:04:51 +0100

    feat: make home responsive

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -8,27 +8,28 @@ export default function Home() {
   return (
     <div>
       <Header />
-      <div className='overflow-hidden h-[200]'>
-        <Image
-          src='/cori.png'
-          width={640}
-          height={640}
-          alt='Ismélio Cori Picture'
-          className=''
-        />
-      </div>
-      <div className='flex justify-center p-5'>
-        <div className='font-mono grid grid-cols-1 gap-5'>
-          <div className='mx-auto'>
-            <h2>Back End Developer</h2>
-            <h1 className='font-sans text-4xl font-extrabold'>
-              Hello I&apos;m <br />{' '}
-              <span className='text-blue'>Ismélio Cori</span>
-            </h1>{' '}
+      <div className='flex mx-auto justify-center p-5 md:grid md:grid-cols-2 md:w-[820]'>
+        <div className='font-mono grid grid-cols-1 gap-4'>
+          <div className='mx-auto sm:grid sm:grid-cols-2 gap-5'>
+            <div className='md:col-span-2'>
+              <h2>Back End Developer</h2>
+              <h1 className='font-sans text-4xl sm:text-6xl font-extrabold'>
+                Hello I&apos;m <br />{' '}
+                <span className='text-blue'>Ismélio Cori</span>
+              </h1>{' '}
+            </div>
+            <div className='overflow-hidden h-[200] sm:h-[210] md:hidden'>
+              <Image
+                src='/cori.png'
+                width={640}
+                height={640}
+                alt='Ismélio Cori Picture'
+                className='sm:rounded-full sm:w-[200]'
+              />
+            </div>
           </div>
-          <p className='text-base text-gray-text'>
+          <p className='text-base text-gray-text md:p-6'>
             Intro text: Lorem ipsum dolor sit amet, consectetur adipiscing elit,
-            sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.{' '}
             sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.{' '}
             sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.{' '}
           </p>
@@ -41,7 +42,14 @@ export default function Home() {
           <div className='text-base text-gray-text mx-auto'>
             <SocialIcons />
           </div>
-        </div>
+        </div>{' '}
+        <Image
+          src='/cori.png'
+          width={350}
+          height={350}
+          alt='Ismélio Cori Picture'
+          className='hidden rounded-full md:block mt-8'
+        />
       </div>
     </div>
   );
--- src/app/ui/social-icons.tsx
+++ src/app/ui/social-icons.tsx
@@ -4,13 +4,13 @@ import { FaTwitter, FaGithub, FaLinkedinIn } from 'react-icons/fa';
 export function SocialIcons() {
   return (
     <div className='flex text-xl'>
-      <Link className='mr-5' href='/'>
+      <Link className='mr-5 hover:text-blue' href='/'>
         <FaLinkedinIn />
       </Link>
-      <Link className='mr-5' href='/'>
+      <Link className='mr-5 hover:text-blue' href='/'>
         <FaGithub />
       </Link>
-      <Link className='mr-5' href='/'>
+      <Link className='mr-5 hover:text-blue' href='/'>
         <FaTwitter />
       </Link>
     </div>
